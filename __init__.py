de7a825c441093d19122eb1d64cd1009cd6ed1a7
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2025-10-31 12:57:48 -0600

    work on the 3d canvas

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
 from typing import TYPE_CHECKING
-
 import wx
 
 from . import monkey_patch
-
 
 # Press and hold `x`, `y` or `z` and then click on one of the points in the plot
 # and drag the point along the axis noted by the key that is being held down.
@@ -14,7 +12,6 @@ from . import monkey_patch
 #
 # Code was added to remove the flicking that was seen when the plot is redrawn.
 
-import wx
 import matplotlib
 
 matplotlib.rcParams[f'axes3d.xaxis.panecolor'] = (0.0, 0.0, 0.0, 0.0)
@@ -38,6 +35,7 @@ from ..wrappers.art3d import Path3DCollection  # NOQA
 
 from mpl_toolkits.mplot3d import axes3d as _axes3d  # NOQA
 import matplotlib.pyplot  # NOQA
+from mpl_toolkits.mplot3d import axes3d  # NOQA
 import numpy as np  # NOQA
 
 from wx.lib.agw import aui  # NOQA
@@ -78,21 +76,53 @@ class Editor3D(wx.Panel):
         self.mainframe: "ui.MainFrame" = parent.GetParent()
         wx.Panel.__init__(self, parent, wx.ID_ANY, style=wx.BORDER_NONE)
 
-        self.fig = matplotlib.pyplot.figure(figsize=(3.5, 3.5))
+        v_sizer = wx.BoxSizer(wx.VERTICAL)
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        ax = self.axes = _axes3d.Axes3D(self.fig, [-0.80, -0.95, 2.8, 2.8])
+        self.fig = matplotlib.pyplot.figure(figsize=(3.5, 3.5))
+        ax = self.axes = axes3d.Axes3D(self.fig, [-0.80, -0.95, 2.8, 2.8], box_aspect=(1, 1, 1))
 
         self.fig.add_axes(ax)
+        # ax.autoscale(True)
 
-        ax.set_xlim3d(-50, 50)
-        ax.set_ylim3d(-50, 50)
-        ax.set_zlim3d(-50, 50)
+        ax.set_xlim(-50, 50)
+        ax.set_ylim(-50, 50)
+        ax.set_zlim(-50, 50)
 
-        inlay = self.inlay = axis_indicator.AxisIndicator(self.fig, Config.axis_indicator)
-        self.canvas = _canvas.Canvas(self, wx.ID_ANY, self.fig, self.axes)
+        # get_tightbbox
+        ax.set_adjustable('datalim')
+        ax.set_aspect('equal', 'box')
 
+        self.canvas = _canvas.Canvas(self.mainframe, self, wx.ID_ANY, self.fig, self.axes)
+
+        inlay = self.inlay = axis_indicator.AxisIndicator(self.fig, [0.88, 0.02, 0.10, 0.10])
         ax.shareview(inlay)
         self.fig.add_axes(inlay)
+
+        hsizer.Add(self.canvas, 1, wx.EXPAND | wx.ALL, 5)
+        v_sizer.Add(hsizer, 1, wx.EXPAND)
+
+        self.SetSizer(v_sizer)
+
+        self.Bind(wx.EVT_SIZE, self._on_panel_size)
+        self.Bind(wx.EVT_MOTION, self._on_motion)
+        self.Bind(wx.EVT_LEFT_DOWN, self._on_left_down)
+        self.Bind(wx.EVT_LEFT_UP, self._on_left_up)
+        # self.Bind(wx.EVT_LEFT_DCLICK, self._on_left_dclick)
+        self.Bind(wx.EVT_RIGHT_DOWN, self._on_right_down)
+        self.Bind(wx.EVT_RIGHT_UP, self._on_right_up)
+        # self.Bind(wx.EVT_RIGHT_DCLICK, self._on_right_dclick)
+        # self.Bind(wx.EVT_MOUSEWHEEL, self._on_mouse_wheel)
+        self.Bind(wx.EVT_KEY_UP, self._on_key_up)
+        self.Bind(wx.EVT_KEY_DOWN, self._on_key_down)
+
+        self.transitions = []
+        self.bundles = []
+        self._selected = None
+        self._mouse_click_location = None
+        self.bundle_dialog = None
+        self._right_motion = None
+        self.canvas.draw()
 
         self.editor3d_toolbar = aui_toolbar.AuiToolBar(self.mainframe, style=aui.AUI_TB_GRIPPER | aui.AUI_TB_TEXT)
         self.editor3d_toolbar.SetToolBitmapSize((48, 48))
@@ -123,7 +153,7 @@ class Editor3D(wx.Panel):
 
         self.buttons = []
 
-        for id, img, label in btns:
+        for id, img, label in btns:  # NOQA
             item = self.editor3d_toolbar.AddTool(id, label, img.bitmap,
                                                  img.disabled_bitmap,
                                                  aui.ITEM_RADIO)
@@ -153,17 +183,6 @@ class Editor3D(wx.Panel):
         self.mainframe.manager.AddPane(self.editor3d_toolbar, self.editor3d_toolbar_pane)
         self.mainframe.manager.Update()
 
-        self.Bind(wx.EVT_SIZE, self.on_size)
-        self.Bind(wx.EVT_MOTION, self.on_motion)
-        self.Bind(wx.EVT_LEFT_DOWN, self.on_left_down)
-        self.Bind(wx.EVT_LEFT_UP, self.on_left_up)
-        self.Bind(wx.EVT_LEFT_DCLICK, self.on_left_dclick)
-        self.Bind(wx.EVT_RIGHT_DOWN, self.on_right_down)
-        self.Bind(wx.EVT_RIGHT_UP, self.on_right_up)
-        self.Bind(wx.EVT_RIGHT_DCLICK, self.on_right_dclick)
-        self.Bind(wx.EVT_KEY_DOWN, self.on_key_down)
-        self.Bind(wx.EVT_KEY_UP, self.on_key_up)
-
         v_sizer = wx.BoxSizer(wx.VERTICAL)
         h_sizer = wx.BoxSizer(wx.HORIZONTAL)
         h_sizer.Add(self.canvas, 1, wx.EXPAND | wx.ALL, 5)
@@ -173,59 +192,80 @@ class Editor3D(wx.Panel):
         def _do():
             self.buttons[0].SetState(aui.AUI_BUTTON_STATE_CHECKED)
 
-            for value in self.axes._axis_map.values():  # NOQA
-                value.set_ticks_position('both')
-                value.set_label_position('both')
-            self.canvas.draw()
-
         wx.CallAfter(_do)
 
-    def on_size(self, evt):
+    def _on_panel_size(self, evt):
         w, h = evt.GetSize()
+        self.canvas.SetSize((w, h))
         size_ = max(w, h)
         axes_off_x = utils.remap(size_, 474, 2333, -1.1, -1.60)
         axes_off_y = utils.remap(size_, 474, 2333, -1.1, -1.4)
         axes_size = utils.remap(size_, 474, 2333, 3.2, 4.20)
-
         self.axes.set_position([axes_off_x, axes_off_y, axes_size, axes_size])
         self.fig.canvas.draw_idle()
+
+        evt.Skip()
+
+    def SetSelected(self, obj, flag):
+        if not flag and self._selected == obj:
+            self._selected.IsSelected(False)
+            self._selected = None
+        elif flag and self._selected is not None and self._selected != obj:
+            self._selected.IsSelected(False)
+            self._selected = obj
+            self._selected.IsSelected(True)
+        elif flag and self._selected is None:
+            self._selected = obj
+            self._selected.IsSelected(True)
+        else:
+            raise RuntimeError('sanity check')
+
+    def _on_motion(self, evt: MouseEvent):
+        if evt.RightIsDown():
+            self._right_motion = True
+
+        evt.Skip()
+
+    def _on_left_down(self, evt: MouseEvent):  # NOQA
+        evt.Skip()
+
+    def _on_left_up(self, evt: MouseEvent):  # NOQA
+        evt.Skip()
+
+    ID_ADD_TRANSITION = wx.NewIdRef()
+    ID_ADD_BUNDLE = wx.NewIdRef()
+
+    def _on_right_down(self, evt: MouseEvent):
+        self._right_motion = False
+        evt.Skip()
+
+    def _on_right_up(self, evt: MouseEvent):
+        evt.Skip()
+
+        if self._right_motion:
+            self._right_motion = False
+            return
+
+        artist = evt.GetArtist()
+        x, y = evt.GetPosition()
+
+        if artist is None:
+            menu = wx.Menu()
+            menu.Append(self.ID_ADD_TRANSITION, "Add Transition")
+            menu.Append(self.ID_ADD_BUNDLE, "Add Bundle")
+
+            self._mouse_click_location = evt.GetPosition3D()
+
+            self.PopupMenu(menu, x, y)
+        else:
+            pass
+            # obj.menu(self, x, y)
+
+    def _on_key_up(self, evt: KeyEvent):  # NOQA
+        evt.Skip()
+
+    def _on_key_down(self, evt: KeyEvent):  # NOQA
         evt.Skip()
 
     def on_tool(self, evt):
         self.mode = evt.GetId()
-
-    def on_motion(self, evt: MouseEvent):
-        evt.Skip()
-
-    def on_left_down(self, evt: MouseEvent):
-        evt.Skip()
-
-    def on_left_up(self, evt: MouseEvent):
-        evt.Skip()
-
-    def on_left_dclick(self, evt: MouseEvent):
-        evt.Skip()
-
-    def on_right_down(self, evt: MouseEvent):
-        evt.Skip()
-
-    def on_right_up(self, evt: MouseEvent):
-        artist = evt.GetArtist()
-        x, y = evt.GetPosition()
-
-        if artist is not None:
-            obj = artist.get_py_data()
-            obj.menu3d(self, x, y)
-
-        evt.Skip()
-
-    def on_right_dclick(self, evt: MouseEvent):
-        evt.Skip()
-
-    def on_key_down(self, evt: KeyEvent):
-        evt.Skip()
-
-    def on_key_up(self, evt: KeyEvent):
-        evt.Skip()
-
-
